27b97b51a17e00a2447b3e20a6ac7caccdac8a84
Rishabh Dalal <dalalr@uni.edu>
2018-05-27 18:24:21 -0500

    Add files via upload

---
+++ BFStandardInterpreter/src/Compiler.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Rishabh Dalal
+ * Description: Class containing main to initialize the interpreter
+ */
+
+public class Compiler {
+	public static void main(String arg[]) throws Exception {
+		try {
+			BFInterpreter standardCompiler = new BFInterpreter();
+		}
+		catch (Exception e) {throw e;}
+	}
+}
